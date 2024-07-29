0dcfa81ad06b8578775850df7dc95ccfc5c008bf
hansolkim9 <masicer2@gmail.com>
2024-07-29 10:34:55 +0900

    # 카트에 상품 추가

--- src/redux-cart/components/Cart/CartItem.js
+++ src/redux-cart/components/Cart/CartItem.js
@@ -1,30 +1,38 @@
 import React from "react";
 import styles from './CartItem.module.css';
+import {useDispatch} from "react-redux";
+import {cartActions} from "../../store/cart-slice";
 
 const CartItem = ({item}) => {
 
-  const { title, quantity, total, price, id } = item;
+    const {title, quantity, total, price, id} = item;
 
-  return (
-    <li className={styles.item}>
-      <header>
-        <h3>{title}</h3>
-        <div className={styles.price}>
-          {total}{' '}
-          <span className={styles.itemprice}>({price}/item)</span>
-        </div>
-      </header>
-      <div className={styles.details}>
-        <div className={styles.quantity}>
-          x <span>{quantity}</span>
-        </div>
-        <div className={styles.actions}>
-          <button>-</button>
-          <button>+</button>
-        </div>
-      </div>
-    </li>
-  );
+    const dispatch = useDispatch();
+
+    const addCartHandler = e => {
+        dispatch(cartActions.addCartItem(item));
+    };
+
+    return (
+        <li className={styles.item}>
+            <header>
+                <h3>{title}</h3>
+                <div className={styles.price}>
+                    {total}{' '}
+                    <span className={styles.itemprice}>({price}/item)</span>
+                </div>
+            </header>
+            <div className={styles.details}>
+                <div className={styles.quantity}>
+                    x <span>{quantity}</span>
+                </div>
+                <div className={styles.actions}>
+                    <button>-</button>
+                    <button onClick={addCartHandler}>+</button>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default CartItem;
